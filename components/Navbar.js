3f529c1addecce340ed5d356c18f9352bce1a4ee
jkrejci1 <jkrejci1988@gmail.com>
2023-07-05 15:19:38 -0500

    new

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         <header>
             <div className="container">
                 <Link to="/">
-                    <h1>Workout Buddy</h1>
+                    <h1>Workout Buddy DUDE DUDE DUDE</h1>
                 </Link>
             </div>
         </header>
